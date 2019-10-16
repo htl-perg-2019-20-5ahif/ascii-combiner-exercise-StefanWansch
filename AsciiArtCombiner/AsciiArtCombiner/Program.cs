94037d74355a13117a370a15085da135d9396e46
Stefan Wansch <stefan-wansch@gmx.at>
2019-10-16 21:04:36 +0200

    Finished

---
+++ AsciiArtCombiner/AsciiArtCombiner/Program.cs
@@ -0,0 +1,56 @@
+﻿using ClassLibraryCombiner;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AsciiArtCombiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFile(args);
+
+        }
+        public static void ReadFile(string[] args)
+        {
+            try
+            {
+                if (args.Length > 1)
+                {
+                    List<string[]> list = new List<string[]>();
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        var splittedFile = ReadFiles(args[i]);
+                        list.Add((string[])splittedFile);
+                    }
+                    Combine(list);
+                }
+                else
+                {
+                    Console.WriteLine("Geben Sie mindestens 2 Dateien an");
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Fehler beim einlesen der Textdatei");
+            }
+        }
+
+        public static void Combine(List<string[]> list)
+        {
+            Combiner c = new Combiner(list);
+            c.combineFiles();
+        }
+
+        public static IEnumerable<string> ReadFiles(string filename)
+        {
+            var file = System.IO.File.ReadAllText(filename);
+            var splittedFile = file.Split('\n');
+            return splittedFile;
+        }
+
+
+
+    }
+}
---
+++ AsciiArtCombiner/ClassLibraryCombiner/Combiner.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibraryCombiner
+{
+    public class Combiner
+    {
+        List<string[]> list;
+
+        public Combiner(List<string[]> list)
+        {
+            this.list = list;
+        }
+
+        public void combineFiles()
+        {
+            
+
+            if (checkRequirements())
+            {
+                String[] result = list[0];
+                foreach (var file in list)
+                {
+                    for (int i = 0; i < result.Length; i++)
+                    {
+                        var fileCharArray = file[i].Replace('\r', ' ').ToCharArray();
+                        var resultCharArray = result[i].Replace('\r', ' ').ToCharArray();
+                        for (int j = 0; j < resultCharArray.Length; j++)
+                        {
+                            if (resultCharArray[j] == ' ' && fileCharArray[j] != ' ')
+                            {
+                                resultCharArray[j] = fileCharArray[j];
+                            }
+                        }
+                        result[i] = new String(resultCharArray);
+                    }
+                }
+                for (int i = 0; i < result.Length; i++)
+                {
+                    Console.WriteLine(result[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dateien stimmen in der Größe nicht überein!");
+            }
+
+            
+        }
+        public Boolean checkRequirements()
+        {
+            foreach (var file in list)
+            {
+                if (file.Length != list[0].Length || file[0].Length != list[0][0].Length)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
